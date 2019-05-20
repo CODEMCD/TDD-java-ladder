47262d67415b6dac69d4d2db82a5366286529042
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-20 15:32:20 +0900

    사다리 타기 구현 2단계 두 번째

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LadderTest {
     @Test
     void runWhenNoLine() {
-        Ladder ladder = new Ladder(3);
+        Ladder ladder = new Ladder(1, 3);
         int target = ladder.run(0);
         assertEquals(0, target);
 
@@ -18,15 +18,15 @@ public class LadderTest {
     @Test
     void runWhenLineLeft() {
         // 0 1 1
-        Ladder ladder = new Ladder(3);
-        ladder.drawLine(1);
+        Ladder ladder = new Ladder(1, 3);
+        ladder.drawLine(0, 1);
 
         int target = ladder.run(2);
         assertEquals(1, target);
 
         // 1 1 0
-        ladder = new Ladder(3);
-        ladder.drawLine(0);
+        ladder = new Ladder(1, 3);
+        ladder.drawLine(0, 0);
 
         target = ladder.run(0);
     }
@@ -34,18 +34,33 @@ public class LadderTest {
     @Test
     void runWhenLineRight() {
         // 0 1 1
-        Ladder ladder = new Ladder(3);
-        ladder.drawLine(1);
+        Ladder ladder = new Ladder(1, 3);
+        ladder.drawLine(0, 1);
 
         int target = ladder.run(1);
         assertEquals(2, target);
 
         // 0 1 1
-        ladder = new Ladder(3);
-        ladder.drawLine(1);
+        ladder = new Ladder(1, 3);
+        ladder.drawLine(0, 1);
 
         target = ladder.run(2);
         assertEquals(1, target);
+    }
 
+    @Test
+    void runWhenMultiRows() {
+        // 1 1 0 0
+        // 0 1 1 0
+        // 0 0 1 1
+        Ladder ladder = new Ladder(3, 4);
+        ladder.drawLine(0, 0);
+        ladder.drawLine(1, 1);
+        ladder.drawLine(2, 2);
+
+        assertEquals(3, ladder.run(0));
+        assertEquals(0, ladder.run(1));
+        assertEquals(1, ladder.run(2));
+        assertEquals(2, ladder.run(3));
     }
 }
