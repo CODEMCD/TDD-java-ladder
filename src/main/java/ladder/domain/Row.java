4dbc1c68f7fb7886608ea571ba352982bc0e14fc
SUNGBUMPARK <psbum1609@gmail.com>
2019-05-20 15:54:41 +0900

    사다리 타기 구현 2단계 세 번째
    * 2단계 두 번째까지는 각 사다리의 위치에서 사다리를 타면 결과 위치를
    반환하는 기능을 구현하였다.
    
    * 2단계 세 전째에서는 이를 리팩토링한다.

---
+++ src/main/java/ladder/domain/Row.java
@@ -0,0 +1,36 @@
+package ladder.domain;
+
+public class Row {
+    int[] persons;
+
+    Row(int noOfPersons) {
+        this.persons = new int[noOfPersons];
+    }
+
+    public void drawLine(int startPosition) {
+        persons[startPosition] = 1;
+        persons[startPosition + 1] = 1;
+    }
+
+    public int moveRow(int nthOfPerson) {
+        if (isNoLine(nthOfPerson)) {
+            return nthOfPerson;
+        }
+        if (isNotLeftEnd(nthOfPerson) && canLeft(nthOfPerson)) {
+            return nthOfPerson - 1;
+        }
+        return nthOfPerson + 1;
+    }
+
+    private boolean isNoLine(int nthOfPerson) {
+        return persons[nthOfPerson] == 0;
+    }
+
+    private boolean isNotLeftEnd(int nthOfPerson) {
+        return nthOfPerson - 1 >= 0;
+    }
+
+    private boolean canLeft(int nthOfPerson) {
+        return persons[nthOfPerson - 1] == 1;
+    }
+}
